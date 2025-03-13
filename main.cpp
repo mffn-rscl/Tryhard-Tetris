286fdd6f2d0693e442ff2cbb39baec59018e0600
Anton Popov <mffnrscl@gmail.com>
2025-03-14 00:44:04 +0200

    added some tetraminos in game

--- main.cpp
+++ main.cpp
@@ -1,18 +1,80 @@
 #include <SFML/Graphics.hpp>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Tryhard Tetris");
+using namespace sf;
+const int M = 20;
+const int N = 20;
 
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+int field[N][M] = { 0 };
 
-        window.clear(sf::Color::Black);
-        window.display();
-    }
+int figures[7][4] = 
+{
+	1, 3, 5, 7,
+	2, 4, 5, 7,
+	3, 5, 4, 6,
+	3, 5, 4, 7,
+	2, 3, 5, 7,
+	3, 5, 7, 6,
+	2, 3, 4, 5,
+};
 
-    return 0;
+struct Point
+{
+	int x,y;
+
+} a[4],b[4];
+
+int main()
+{
+	RenderWindow window(VideoMode(580,640), "Adrew Shotgun",sf::Style::Titlebar | sf::Style::Close);
+	
+	Texture tetramino_texture;
+	tetramino_texture.loadFromFile("textures/tetramino.png");
+
+	while (window.isOpen())
+	{
+		Event event;
+		while(window.pollEvent(event))
+		{
+			if (event.type == Event::Closed) window.close();
+		}
+		window.clear(Color(23, 22,34));
+		
+		Sprite tetramino(tetramino_texture);
+		tetramino.setScale(0.2f, 0.2f);
+	
+		int n = 3;
+		for (int i = 0; i < 4; i++)
+		{
+			a[i].x = figures[n][i] % 2;
+			a[i].y = figures[n][i] / 2;
+
+		/*
+			a[0]: (1, 1)
+			a[1]: (1, 2)
+			a[2]: (0, 2)
+			a[3]: (1, 3)
+		*/
+		}
+		
+		for (int i = 0; i < 4; i++)
+		{	
+			tetramino.setTextureRect(IntRect(0, 0, 161, 161));
+			tetramino.setPosition(a[i].x * 32, a[i].y * 32);
+
+			/*
+				a[0]: (32, 32)
+				a[1]: (64, 64)
+				a[2]: (0, 64)
+				a[3]: (32, 96)
+			
+			*/
+
+			window.draw(tetramino);
+
+
+		}
+		
+		window.display();
+	}
+	return 0;
 }
